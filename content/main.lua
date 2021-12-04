cc1e554582d4542a6588f0d658f32428463d15b3
Jérémy Vasseur <44971443+anxor@users.noreply.github.com>
2021-12-04 13:39:52 +0100

    Add empty ux core

--- content/main.lua
+++ content/main.lua
@@ -1,9 +1,10 @@
 require("source/state")
 require("source/utils")
+require('source/ux_core')
 math.randomseed(os.time())
 
 function love.draw()
-
+    ux_core_draw()
 end
 
 --
@@ -11,10 +12,12 @@ end
 function love.load()
     initialize_state()
     print_table(game_state)
+    ux_core_load()
 end
 
 --
 
 function love.update(dt)
     update_state()
+    ux_core_update(dt)
 end
---
+++ content/source/ux_core.lua
@@ -0,0 +1,16 @@
+
+function ux_core_draw()
+
+end
+
+--
+
+function ux_core_load()
+
+end
+
+--
+
+function ux_core_update(dt)
+
+end
