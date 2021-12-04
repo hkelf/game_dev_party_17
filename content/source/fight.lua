8f64cb9d21fe08d4511cecd577f722cf8af130da
hkelf <hugo.kelfani@gmail.com>
2021-12-04 13:21:48 +0100

    Adding missing files lul

---
+++ content/source/fight.lua
@@ -0,0 +1,38 @@
+require("source/broker")
+require("source/utils")
+reauire("source/fight_player_turn")
+
+local select_ennemy = function() 
+    local rnd = math.random(#game_state.ennemy_pool)
+    print(game_state.ennemy_pool[rnd].id)
+    game_state.current_ennemy = table.clone(game_state.ennemy_pool[rnd])
+    print(game_state.current_ennemy)
+    if not game_state.already_fought_ennemies 
+    then 
+        game_state.already_fought_ennemies = {} 
+    end
+    print("SELECTED ENNEMY:")
+    print_table(game_state.current_ennemy)
+    game_state.already_fought_ennemies[game_state.current_ennemy.id] = true
+
+    broker_send("ennemy_created", {sender="fight", payload=game_state.current_ennemy})
+end
+
+function init_fight() 
+    game_state.scene = {
+        type="FIGHT",
+        phase="PLAYER_TURN"
+    }
+
+    select_ennemy()
+    print("FIGHT_STARTED\n")
+    print_table(game_state)
+    print("\n")
+end
+
+function update_fight()
+    if game_state.scene.phase = "PLAYER_TURN" then
+        process_player_turn()
+    end
+end
+
---
+++ content/source/fight_player_turn.lua
@@ -0,0 +1,15 @@
+local init_flee = function()
+    
+end
+
+local init_skill = function()
+
+end
+
+function process_player_turn() 
+    if game_state.selected.flee then
+        init_flee()
+    else if game_state.selected.skill then
+        init_skill()
+    end
+end
